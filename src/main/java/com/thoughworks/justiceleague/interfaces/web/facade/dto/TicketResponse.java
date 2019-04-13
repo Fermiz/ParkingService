6c2766c8cea915771228b94dd0a4137ce7de1957
gitqh <quhang19881208@@163.com>
2019-04-13 16:02:22 +0800

    fix

--- src/main/java/com/thoughworks/justiceleague/interfaces/web/facade/dto/TicketResponse.java
+++ src/main/java/com/thoughworks/justiceleague/interfaces/web/facade/dto/TicketResponse.java
@@ -1,12 +1,16 @@
 package com.thoughworks.justiceleague.interfaces.web.facade.dto;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
-@AllArgsConstructor
 public class TicketResponse {
     private String ticketId;
+
+    @JsonCreator
+    public TicketResponse(String ticketId) {
+        this.ticketId = ticketId;
+    }
 }
