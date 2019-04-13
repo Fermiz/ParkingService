68c199192c6021b3067f851425504a2766d014ab
gitqh <quhang19881208@@163.com>
2019-04-13 15:50:46 +0800

    fix serilizable question

--- src/main/java/com/thoughworks/justiceleague/interfaces/web/facade/dto/TicketResponse.java
+++ src/main/java/com/thoughworks/justiceleague/interfaces/web/facade/dto/TicketResponse.java
@@ -2,9 +2,11 @@ package com.thoughworks.justiceleague.interfaces.web.facade.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
 public class TicketResponse {
     private String ticketId;
 }
